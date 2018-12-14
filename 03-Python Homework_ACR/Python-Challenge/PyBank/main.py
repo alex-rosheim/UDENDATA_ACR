13dce79a56fee581f68833dd0dd96f829d6dbed4
alex rosheim <alex.rosheim@gmail.com>
2018-12-13 21:45:57 -0700

    adding txt file(s)

---
+++ 03-Python Homework_ACR/Python-Challenge/PyBank/main.py
@@ -0,0 +1,44 @@
+import csv
+import numpy as np
+
+total_profit = 0
+profit_list = []
+profits = []
+
+with open("budget_data.csv", 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    for row in reader:
+        date, profit = row #unpack the row
+        row_profit= int(profit)
+        profit_list.append(row_profit)
+        profits.append(profit)
+        total_profit += row_profit
+        
+total_months = len(profit_list)
+increase = (max(profit_list)) #should be 1170593
+decrease = (min(profit_list)) #should be -1196225
+
+pairs = []
+for index, row in enumerate(profits):
+    try:
+        pairs.append((int(profits[index + 1]), int(row)))
+    except IndexError:
+        pass
+    
+month_diff = []
+
+for pair in pairs:
+    second, first = pair
+    diff = (int(second) - int(first))
+    month_diff.append(diff)
+
+with open('PyBank.txt', 'w') as f:
+    
+    f.write("Financial Analysis \n")
+    f.write("---------------------------- \n")  
+    f.write("Total Months : " + str(total_months) + "\n") #should be 86 months
+    f.write("Total Profit : " + str(total_profit) + "\n") #should be 38382578  
+    f.write("Average Change : " + str(np.mean(month_diff)) + "\n") #should be -2315.12
+    f.write("Greatest Increase in Profits : " + str(increase) + "\n") 
+    f.write("Greatest Decrease in Profits : " + str(decrease) + "\n")
---
+++ 03-Python Homework_ACR/Python-Challenge/PyPoll/main.py
@@ -0,0 +1,58 @@
+import csv
+
+votes = []
+
+with open("election_data.csv", 'r') as f:
+    reader = csv.reader(f, delimiter=',')
+    next(reader)
+    for row in reader:
+        voter_id, county, candidate = row
+        votes.append(candidate) 
+        
+total_votes = len(votes)
+candidates = (set(votes))
+kahn_counter = 0
+li_counter = 0
+correy_counter = 0
+otooley_counter = 0
+
+# print(candidates) = {"O'Tooley", 'Correy', 'Khan', 'Li'}
+
+for x in votes:
+    if x == ("Khan"):
+        kahn_counter += 1
+    elif x == ("Li"):
+        li_counter += 1 
+    elif x == ("Correy"):
+        correy_counter += 1 
+    elif x == ("O'Tooley"):
+        otooley_counter += 1       
+
+counter = [int(otooley_counter),int(correy_counter),int(kahn_counter),int(li_counter)]
+total_counter = dict(zip(candidates,counter))
+percentage_summary = {k: v / total_votes for k,v in total_counter.items()}
+
+
+with open('PyPoll.txt', 'w') as f:
+
+    f.write("Election Results \n")
+    f.write("---------------------------- \n")
+    f.write("Total Number of Votes: \n")
+    f.write(str(total_votes) + "\n")
+    f.write("---------------------------- \n")
+    f.write("Canidates Receiving Votes: \n")
+    for cand, pct in percentage_summary.items():
+        f.write (f"{cand}" + "\n")
+    f.write("---------------------------- \n")
+    f.write("Total Number Votes per Candidate: \n")
+    f.write((list(candidates))[0] + " : " + str(otooley_counter) + "\n")
+    f.write((list(candidates))[1] + " : " + str(correy_counter) + "\n")
+    f.write((list(candidates))[2] + " : " + str(kahn_counter) + "\n")
+    f.write((list(candidates))[3] + " : " + str(li_counter) + "\n")
+    f.write("---------------------------- \n")
+    f.write("Percentage of Votes per Candidate: \n")
+    for cand, pct in percentage_summary.items():
+        f.write (f"{cand} : {round(pct *100,2)}% \n")
+    f.write("---------------------------- \n")
+    f.write("Winner of Election : " + (str(max(percentage_summary, key=percentage_summary.get)) + "\n"))
+
